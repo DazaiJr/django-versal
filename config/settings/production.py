1b6485112e0466fc9840a26f2c867a432bae14c4
yash <ykshirsagar554@gmail.com>
2026-02-15 21:07:22 +0530

    updated env variable names according to railay updated6

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,15 +1,24 @@
 from .base import *
 import os
+import dj_database_url
 
 DEBUG = False
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
+}
+
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 USE_X_FORWARDED_HOST = True
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://django-versal-production.up.railway.app",
+    f"https://{host}" for host in ALLOWED_HOSTS if host
 ]
 
 CSRF_COOKIE_SECURE = True
