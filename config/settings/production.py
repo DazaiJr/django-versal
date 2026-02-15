6f37e5d9e83d5d5f53560c09e268cbda6d690b19
yash <ykshirsagar554@gmail.com>
2026-02-15 20:41:43 +0530

    updated env variable names according to railay updated4

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,16 +1,17 @@
-import dj_database_url
 from .base import *
 
 DEBUG = False
+
 ALLOWED_HOSTS = ["*"]
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=env("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True,
-    )
-}
-
+# Railway runs behind HTTPS proxy
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-SECURE_SSL_REDIRECT = False
+USE_X_FORWARDED_HOST = True
+
+# CSRF settings (THIS FIXES ADMIN LOGIN)
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.railway.app",
+]
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
